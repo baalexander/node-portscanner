e232efc85fa694735ce4430d787d970380987de1
Brandon Alexander <baalexander@gmail.com>
2011-08-11 17:04:43 -0500

    Checks a range of ports for first open or closed port.

--- example/portscan.js
+++ example/portscan.js
@@ -4,8 +4,12 @@ portscanner.checkPortStatus('3000', 'localhost', function(error, status) {
   console.log(status)
 })
 
-portscanner.findAnAvailablePort(3000, 3010, 'localhost', function(error, port) {
-  console.log('AVAILABLE PORT AT ' + port)
+portscanner.findAnOpenPort(3000, 3010, 'localhost', function(error, port) {
+  console.log('OPEN PORT AT ' + port)
+})
+
+portscanner.findAClosedPort(3000, 3010, 'localhost', function(error, port) {
+  console.log('CLOSED PORT AT ' + port)
 })
 
 setTimeout(function() { }, 10000)
