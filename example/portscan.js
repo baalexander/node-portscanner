8c1f11e76c1ab0cabb687532be98d21ac6614dd1
Brandon Alexander <baalexander@gmail.com>
2011-08-28 22:21:24 -0500

    Adds JSDocs and updates example code.

--- example/portscan.js
+++ example/portscan.js
@@ -1,16 +1,38 @@
-var portscanner = require('../lib/portscanner.js')
+var http        = require('http')
+  , portscanner = require('../lib/portscanner.js')
 
-portscanner.checkPortStatus(3000, 'localhost', function(error, status) {
-  console.log(status)
+// Sets up an HTTP server listening on port 3005
+var server = http.createServer(function (request, response) {
+
 })
+server.listen(3005, 'localhost')
 
-portscanner.findAnOpenPort(3000, 3010, 'localhost', function(error, port) {
-  console.log('OPEN PORT AT ' + port)
-})
+// Waits briefly before port scanning to let the HTTP server start listening
+setTimeout(function() {
 
-portscanner.findAClosedPort(3000, 3010, 'localhost', function(error, port) {
-  console.log('CLOSED PORT AT ' + port)
-})
+  // Checks the status of an individual port.
+  portscanner.checkPortStatus(3005, 'localhost', function(error, status) {
+    // Status should be 'open' since the HTTP server is listening on that port
+    console.log('Status at port 3005 is ' + status)
+  })
 
-setTimeout(function() { }, 10000)
+  portscanner.checkPortStatus(3000, 'localhost', function(error, status) {
+    // Status should be 'closed' since no service is listening on that port.
+    console.log('Status at port 3000 is ' + status)
+  })
+
+  // Finds a port that a service is listening on
+  portscanner.findAnOpenPort(3000, 3010, 'localhost', function(error, port) {
+    // Port should be 3005 as the HTTP server is listening on that port
+    console.log('Found an open port at ' + port)
+  })
+
+  // Finds a port that is not currently in use
+  portscanner.findAClosedPort(3000, 3010, 'localhost', function(error, port) {
+    // Will return any number between 3000 and 3010 (inclusive), that's not 3005.
+    // The order is unknown as the port status checks are asynchronous.
+    console.log('Found a closed port at ' + port)
+  })
+
+}, 500)
 
