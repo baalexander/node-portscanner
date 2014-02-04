0b72b83cab42c3c4e23fe472ae2719600e69a88b
Sean Massa <smassa@groupon.com>
2014-02-04 17:03:24 -0600

    switch out localhost for 127.0.0.1

--- example/portscan.js
+++ example/portscan.js
@@ -5,27 +5,27 @@ var http        = require('http')
 var server = http.createServer(function (request, response) {
 
 })
-server.listen(3005, 'localhost', function() {
+server.listen(3005, '127.0.0.1', function() {
 
   // Checks the status of an individual port.
-  portscanner.checkPortStatus(3005, 'localhost', function(error, status) {
+  portscanner.checkPortStatus(3005, '127.0.0.1', function(error, status) {
     // Status should be 'open' since the HTTP server is listening on that port
     console.log('Status at port 3005 is ' + status)
   })
 
-  portscanner.checkPortStatus(3000, 'localhost', function(error, status) {
+  portscanner.checkPortStatus(3000, '127.0.0.1', function(error, status) {
     // Status should be 'closed' since no service is listening on that port.
     console.log('Status at port 3000 is ' + status)
   })
 
   // Finds a port that a service is listening on
-  portscanner.findAPortInUse(3000, 3010, 'localhost', function(error, port) {
+  portscanner.findAPortInUse(3000, 3010, '127.0.0.1', function(error, port) {
     // Port should be 3005 as the HTTP server is listening on that port
     console.log('Found an open port at ' + port)
   })
 
   // Finds a port no service is listening on
-  portscanner.findAPortNotInUse(3000, 3010, 'localhost', function(error, port) {
+  portscanner.findAPortNotInUse(3000, 3010, '127.0.0.1', function(error, port) {
     // Will return any number between 3000 and 3010 (inclusive), that's not 3005.
     // The order is unknown as the port status checks are asynchronous.
     console.log('Found a closed port at ' + port)
