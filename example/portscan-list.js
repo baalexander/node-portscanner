5a714b265b36a281cde6b836c08bb34f30f4f2f7
darthneko <kuroneko_no_malkavian@hotmail.com>
2015-01-28 17:30:31 +0100

    Detect Arrays from original API
    Now the original API works with both arrays and start/end ports.
    Detection on which method to use is based on argument number.

--- example/portscan-list.js
+++ example/portscan-list.js
@@ -2,7 +2,7 @@ var http        = require('http')
   , portscanner = require('../lib/portscanner.js')
 
 // Check if one of the ports in the list is available
-portscanner.findAPortNotInUseInList([4443, 4444, 4447], '127.0.0.1', function(error, port) {
+portscanner.findAPortNotInUse([4443, 4444, 4447], '127.0.0.1', function(error, port) {
 	if (!error) {
 		console.log('Available port: ' + port);
 	} else {
