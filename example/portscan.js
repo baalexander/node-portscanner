7a5d53fbaa40126b1738c211a3678ceb3760df83
Yanis Benson <yanis.benson@gmail.com>
2015-02-23 13:34:17 +0300

    cleaned up port iteration, added methods to check ports from array, checking for proper port range closes baalexander/node-portscanner#23, and closes baalexander/node-portscanner#26

--- example/portscan.js
+++ example/portscan.js
@@ -19,7 +19,7 @@ server.listen(3005, '127.0.0.1', function() {
   })
 
   // Finds a port that a service is listening on
-  portscanner.findAPortInUse(3000, 3010, '127.0.0.1', function(error, port) {
+  portscanner.findAPortInUse(3010, 3000, '127.0.0.1', function(error, port) {
     // Port should be 3005 as the HTTP server is listening on that port
     console.log('Found an open port at ' + port)
     if (error) console.error(error);
@@ -29,6 +29,21 @@ server.listen(3005, '127.0.0.1', function() {
     // Will return any number between 3000 and 3010 (inclusive), that's not 3005.
     // The order is unknown as the port status checks are asynchronous.
     console.log('Found a closed port at ' + port)
+    if (error) console.error(error);
+  })
+
+  var list = [3000,3001,3002,3003,3004,3005,3006,3007,3008,3009,3010];
+  // Finds a port that a service is listening on
+  portscanner.findAPortInUseFromList(list, '127.0.0.1', function(error, port) {
+    // Port should be 3005 as the HTTP server is listening on that port
+    console.log('Found an open port of '+list+' at ' + port)
+    if (error) console.error(error);
+  })
+  // Finds a port no service is listening on
+  portscanner.findAPortNotInUseFromList(list, '127.0.0.1', function(error, port) {
+    // Will return any number between 3000 and 3010 (inclusive), that's not 3005.
+    // The order is unknown as the port status checks are asynchronous.
+    console.log('Found a closed port of '+list+' at ' + port)
     if (error) console.error(error);
   })
 })
